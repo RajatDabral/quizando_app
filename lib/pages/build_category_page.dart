7542d62a4e26325d357c67e09c04485914bebd65
Rajat Dabral <dabralrajat4444@gmail.com>
2020-04-29 15:55:01 +0530

    updated buildCategoryPage UI

--- lib/pages/build_category_page.dart
+++ lib/pages/build_category_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:gradient_app_bar/gradient_app_bar.dart';
+import 'package:personality_app/providers/quiz_provider.dart';
 import '../providers/categories.dart';
 import 'package:provider/provider.dart';
 
@@ -8,20 +10,55 @@ class BuildCategoryPage {
 
   BuildCategoryPage({this.context, this.id});
 
+  Widget raisedButton(String text, Difficulty d, int id) {
+    final quizProvider = Provider.of<QuizProvider>(context, listen: false);
+    return RaisedButton(
+      onPressed: () {
+        quizProvider.selectDifficulty(d, id);
+      },
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(4.0),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+
   Widget buildCatPage() {
     final catProvider =
         Provider.of<Categories>(context, listen: false).categoryItems;
+
     final _currentCatItem =
         catProvider.firstWhere((catItem) => catItem.id == id);
+
     return Scaffold(
-      // backgroundColor: Colors.blueGrey,
+      appBar: GradientAppBar(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xFF8B4BAF),
+            Color(0xFF914EE5),
+            Color(0xFF3A9ECD),
+            Color(0xFFE36AE1),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.centerRight,
+        ),
+        elevation: 0,
+        title: Text(_currentCatItem.name),
+        centerTitle: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
       body: SafeArea(
         child: Container(
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 20),
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
           decoration: BoxDecoration(
-            // borderRadius: BorderRadius.circular(40.0),
             gradient: LinearGradient(
               colors: [
                 Color(0xFF8B4BAF),
@@ -36,7 +73,8 @@ class BuildCategoryPage {
           child: Column(
             children: <Widget>[
               Container(
-                height: MediaQuery.of(context).size.height * .45,
+                // margin: EdgeInsets.symmetric(horizontal: 8),
+                height: MediaQuery.of(context).size.height * .40,
                 width: MediaQuery.of(context).size.width,
                 decoration: BoxDecoration(
                   image: DecorationImage(
@@ -45,7 +83,36 @@ class BuildCategoryPage {
                     ),
                     fit: BoxFit.cover,
                   ),
-                  borderRadius: BorderRadius.circular(40.0),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "How much can you take it",
+                      style: TextStyle(fontSize: 30),
+                    ),
+                    SizedBox(height: 20),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          raisedButton(
+                              'NOOB', Difficulty.Easy, _currentCatItem.id),
+                          raisedButton(
+                              'PRO', Difficulty.Medium, _currentCatItem.id),
+                          raisedButton(
+                              'MASTER', Difficulty.Hard, _currentCatItem.id),
+                        ],
+                      ),
+                    )
+                  ],
                 ),
               ),
             ],
--- lib/providers/quiz_provider.dart
+++ lib/providers/quiz_provider.dart
@@ -1,15 +1,28 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-
 import 'package:http/http.dart' as http;
 
+enum Difficulty { Easy, Medium, Hard }
+
 class QuizProvider with ChangeNotifier {
+   String diff;
+   int id;
+
+  void selectDifficulty(Difficulty d, int id) {
+    if (d == Difficulty.Easy) diff = "easy";
+    if (d == Difficulty.Medium) diff = "medium";
+    if (d == Difficulty.Hard) diff = "hard";
+    print(diff);  //testing
+    this.id = id;
+    print(this.id);
+  }
+
   Future<void> getData() async {
     // const url = "https://opentdb.com/api.php?amount=10";
     // const url = "https://opentdb.com/api_category.php";
     try {
-      const url =
-          "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple";
+      final url =
+          "https://opentdb.com/api.php?amount=10&category=$id&difficulty=$diff&type=multiple";
       final response = await http.get(url);
       final data = json.decode(response.body);
       print(data);
